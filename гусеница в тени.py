b06e533c1cccb182bd3549931a7785003601dd65
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2022-11-26 19:03:35 +0300

    Create гусеница в тени.py

---
+++ гусеница в тени.py
@@ -0,0 +1,147 @@
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import math
+
+pygame.init()
+display = (1200, 900)
+scree = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+glEnable(GL_DEPTH_TEST)
+glEnable(GL_LIGHTING)
+glShadeModel(GL_SMOOTH)
+glEnable(GL_COLOR_MATERIAL)
+glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+
+glEnable(GL_LIGHT0)
+glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.5, 1])
+glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0, 1.0, 1.0, 1])
+
+sphere = gluNewQuadric()
+
+glMatrixMode(GL_PROJECTION)
+gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+
+glMatrixMode(GL_MODELVIEW)
+gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1)
+viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+glLoadIdentity()
+displayCenter = [scree.get_size()[i] // 2 for i in range(2)]
+mouseMove = [0, 0]
+pygame.mouse.set_pos(displayCenter)
+
+pygame.mouse.set_visible(False)
+up_down_angle = 0.0
+paused = False
+run = True
+
+ambient = (0,0,0,1)
+lightpos = (2,2,2)
+z=0
+#4 строки для освещения
+glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient)
+glEnable(GL_LIGHTING)
+glEnable(GL_LIGHT0)
+glLightfv(GL_LIGHT0,GL_POSITION,lightpos)
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
+                run = False
+            if event.key == pygame.K_PAUSE or event.key == pygame.K_p:
+                paused = not paused
+                pygame.mouse.set_pos(displayCenter)
+        if not paused:
+            if event.type == pygame.MOUSEMOTION:
+                mouseMove = [event.pos[i] - displayCenter[i] for i in range(2)]
+            pygame.mouse.set_pos(displayCenter)
+
+    if not paused:
+        # get keys
+        keypress = pygame.key.get_pressed()
+        # mouseMove = pygame.mouse.get_rel()
+
+        # init model view matrix
+        glLoadIdentity()
+
+        # apply the look up and down
+        up_down_angle += mouseMove[1] * 0.1
+        glRotatef(up_down_angle, 1.0, 0.0, 0.0)
+
+        # init the view matrix
+        glPushMatrix()
+        glLoadIdentity()
+        ms = 0.06
+        # apply the movment
+        if keypress[pygame.K_w]:
+            glTranslatef(0, 0, ms)
+        if keypress[pygame.K_s]:
+            glTranslatef(0, 0, -ms)
+        if keypress[pygame.K_d]:
+            glTranslatef(-ms, 0, 0)
+        if keypress[pygame.K_a]:
+            glTranslatef(ms, 0, 0)
+
+        # apply the left and right rotation
+        glRotatef(mouseMove[0] * 0.1, 0.0, 1.0, 0.0)
+
+    # multiply the current matrix by the get the new view matrix and store the final vie matrix 
+    glMultMatrixf(viewMatrix)
+    viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+
+    # apply view matrix
+    glPopMatrix()
+    glMultMatrixf(viewMatrix)
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) #Clear the screen
+    lightpos = (2.0*math.cos(z), -2.0*math.cos(z), 2.0)
+    z += 0.1
+
+    glPushMatrix()
+
+    glLightfv(GL_LIGHT0, GL_POSITION, lightpos)
+
+    glColor3f(0.130, 0, 0)
+    rad = 1
+    for i in range(14):
+        glTranslatef(0, 0, 0.1)
+        rad-=0.02
+
+
+    glTranslatef(-1.5, 0, 0) #Move to the place
+    glColor3f(0, 0.5, 0.5) #Put color
+    gluSphere(sphere, 1.0, 32, 16) #Draw sphere
+    glTranslatef(-1.8, 0, 0) #Move to the place
+    gluSphere(sphere, 1.0, 32, 16) #Draw sphere
+    glTranslatef(-1.9, 0, 0) #Move to the place
+    gluSphere(sphere, 1.0, 32, 16) #Draw sphere
+    glColor4f(1, 1, 1, 1) #Put color
+    glTranslatef(-1, 0.5, 0.2) #Move to the place
+    gluSphere(sphere, 0.2, 32, 16) #Draw sphere
+    glTranslatef(0, -1, 0) #Move to the place
+    gluSphere(sphere, 0.2, 32, 16) #Draw sphere
+    glTranslatef(0, 0, 0) #Move to the place
+    gluSphere(sphere, 0.2, 32, 16) #Draw sphere
+    glColor4f(0, 0, 0, 0) #Put color
+    glTranslatef(-0.19, 0, 0) #Move to the place
+    gluSphere(sphere, 0.02, 32, 16) #Draw sphere
+    glTranslatef(-0.01, 1, 0) #Move to the place
+    gluSphere(sphere, 0.02, 32, 16) #Draw sphere
+    glColor4f(1, 0, 0, 0) #Put color
+    glTranslatef(0.2, -0.5, -0.2) #Move to the place
+    gluSphere(sphere, 0.2, 32, 16) #Draw sphere
+
+    
+
+    glPopMatrix()
+
+    pygame.display.flip() #Update the screen
+    pygame.time.wait(10)
+
+pygame.quit()
